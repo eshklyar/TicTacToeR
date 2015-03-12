4d692ed62df8f3bb1a04d3ed59e290a46f9087ed
eshklyar <eshklyar@gmail.com>
2015-03-12 17:04:28 -0500

    Initial Commit

---
+++ TicTacToeR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToeR
+//
+//  Created by Edik Shklyar on 3/12/15.
+//  Copyright (c) 2015 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToeR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToeR
+//
+//  Created by Edik Shklyar on 3/12/15.
+//  Copyright (c) 2015 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
